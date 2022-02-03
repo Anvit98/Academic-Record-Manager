137b52c5216b4d6e14ca385f72f2292025630be3
Anvit98 <46262206+Anvit98@users.noreply.github.com>
2022-02-04 00:19:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -115,30 +115,4 @@ def start():
         except ValueError:
             print("Please enter input as suggested.")
 start()
-        
-'''connection = sqlite3.connect("Academic Record Manager.db")
-  
-# cursor
-crsr = connection.cursor()
-  
-# print statement will execute if there
-# are no errors
-print("Connected to the database")
-
-#crsr = connection.cursor()
-  
-# execute the command to fetch all the data from the table emp
-#crsr.execute("SELECT * FROM emp")
-  
-# store all the fetched data in the ans variable
-#ans = crsr.fetchall()
-  
-# Since we have already selected all the data entries
-# using the "SELECT *" SQL command and stored them in
-# the ans variable, all we need to do now is to print
-# out the ans variable
-#for i in ans:
-#    print(i)
-  
-# close the connection
-connection.close()'''
+       
